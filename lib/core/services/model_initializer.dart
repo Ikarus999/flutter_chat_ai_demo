c318558cf30c7963d1846cd694d4564d101df0d4
Abdul Samad <32610065+Ikarus999@users.noreply.github.com>
2025-05-25 15:22:44 +0500

    Update model_initializer.dart

--- lib/core/services/model_initializer.dart
+++ lib/core/services/model_initializer.dart
@@ -1,21 +1,43 @@
-import 'package:flutter_mediapipe_chat/flutter_mediapipe_chat.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gemma/core/model.dart';
+import 'package:flutter_gemma/flutter_gemma.dart';
+import 'package:flutter_gemma/pigeon.g.dart';
 import 'package:tflite_demo/core/utils/app_assets.dart';
-import 'package:tflite_flutter/tflite_flutter.dart';
 
 class ModelInitializer {
-  final chatPlugin = FlutterMediapipeChat();
-  late final Interpreter interpreter;
-  final config = ModelConfig(
-    path: AppAssets.modelTaskPath,
-    temperature: 0.7,
-    maxTokens: 1024,
-    topK: 50,
-    randomSeed: 42,
-    loraPath: null,
-  );
+  final gemma = FlutterGemmaPlugin.instance;
+  late final InferenceModel? inferenceModel;
+  late final InferenceModelSession? session;
+
+  late final String? response, response1;
 
   void initModel() async {
-    interpreter = await Interpreter.fromAsset(AppAssets.modelPath);
-    await chatPlugin.loadModel(config);
+    try {
+      await gemma.modelManager.setModelPath(AppAssets.modelTaskPath);
+      gemma.modelManager.installModelFromAsset(AppAssets.modelTaskPath);
+      if (await gemma.modelManager.isModelInstalled) {
+        inferenceModel = await gemma.createModel(
+            modelType: ModelType.gemmaIt,
+            maxTokens: 1024,
+            preferredBackend: PreferredBackend.gpu);
+
+        debugPrint(inferenceModel.toString());
+        session = await inferenceModel?.createSession(
+          temperature: 0.8, // Optional, default is 0.8
+          randomSeed: 1, // Optional, default is 1
+          topK: 1, // Optional, default is 1
+        );
+
+        session?.addQueryChunk(Message(text: 'What are you called?'));
+        response = await session?.getResponse();
+        debugPrint(response);
+        session?.addQueryChunk(
+            Message(text: 'Will you be sufficient for a small AI chat app?'));
+        response1 = await session?.getResponse();
+        debugPrint(response1);
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 }
